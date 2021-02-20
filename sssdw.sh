54271b088cd0460e23a7c521f193473b2dcb3d36
Igor Leszczyński <36318218+Dankage102@users.noreply.github.com>
2021-02-20 17:28:58 +0000

    Update sssdw.sh

--- sssdw.sh
+++ sssdw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#watch -n 180 sh ./sssdw.sh
+#watch -n 180 sh ./sssdw.sh (Use this command to run the script)
 
 # -X POST - use POST method.
 # -H "Content-Type: application/json" - header tells server you're sending JSON data.
@@ -10,27 +10,33 @@
 # and set webhook url as variable, so you don't need to paste it over and over again.
 # Also you can add it to your `.*rc` file.
 
-_CompareString="Players:";
+_CompareString='Players:';
 _Delimiter='```';
 
-_PlayersOnline="`wget -qO- https://api.soldat.pl/v0/server/ip/port/players`"
-_PlayersOnline=`echo $_PlayersOnline | tr -dc ':,[:space:][:alnum:]\n\r'`
-_PlayersOnline=`echo $_PlayersOnline | sed 's/,/, /g'`
-if [ "$_PlayersOnline" != "$_CompareString" ]; then
-export WEBHOOK_URL="webhook link here"
+_PlayersOnline=$(wget -qO- https://api.soldat.pl/v0/server/ip/port/players)
+_PlayersOnline=$(echo $_PlayersOnline | tr -dc ':,[:space:][:alnum:]\n\r')
+_PlayersOnline=$(echo $_PlayersOnline | sed 's/,/, /g')
+if [ -n "$_PlayersOnline" ] && [ "$_PlayersOnline" != "$_CompareString" ]; then
 curl -X POST \
   -H "Content-Type: application/json" \
-  -d '{"username": "Server#1", "content": "'"$_Delimiter$_PlayersOnline$_Delimiter"'"}' \
-  $WEBHOOK_URL
+  -d '{"username": "Server#1<", "content": "'"$_Delimiter$_PlayersOnline$_Delimiter"'"}' \
+  "webhook link here"
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"username": "Server#1>", "content": "_ _"}' \
+  "webhook link here"
 fi
 
-_PlayersOnline="`wget -qO- https://api.soldat.pl/v0/server/ip/port/players`"
-_PlayersOnline=`echo $_PlayersOnline | tr -dc ':,[:space:][:alnum:]\n\r'`
-_PlayersOnline=`echo $_PlayersOnline | sed 's/,/, /g'`
-if [ "$_PlayersOnline" != "$_CompareString" ]; then
-export WEBHOOK_URL="webhook link here"
+_PlayersOnline=$(wget -qO- https://api.soldat.pl/v0/server/ip/port/players)
+_PlayersOnline=$(echo $_PlayersOnline | tr -dc ':,[:space:][:alnum:]\n\r')
+_PlayersOnline=$(echo $_PlayersOnline | sed 's/,/, /g')
+if [ -n "$_PlayersOnline" ] && [ "$_PlayersOnline" != "$_CompareString" ]; then
 curl -X POST \
   -H "Content-Type: application/json" \
-  -d '{"username": "Server#2", "content": "'"$_Delimiter$_PlayersOnline$_Delimiter"'"}' \
-  $WEBHOOK_URL
+  -d '{"username": "Server#2<", "content": "'"$_Delimiter$_PlayersOnline$_Delimiter"'"}' \
+  "webhook link here"
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"username": "Server#2>", "content": "_ _"}' \
+  "webhook link here"
 fi
